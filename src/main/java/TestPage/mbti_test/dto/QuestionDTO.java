2fe6e82b4c26b9ff76fc83aec638506fb3dfe31e
Shs0160 <aladinsue@gmail.com>
2025-01-17 16:43:04 +0900

    Question DTO 생성

---
+++ src/main/java/TestPage/mbti_test/dto/QuestionDTO.java
@@ -0,0 +1,121 @@
+package TestPage.mbti_test.dto;
+
+public class QuestionDTO {
+    private Long userId;
+    private Long num1;
+    private Long num2;
+    private Long num3;
+    private Long num4;
+    private Long num5;
+    private Long num6;
+    private Long num7;
+    private Long num8;
+    private Long num9;
+    private Long num10;
+    private Long num11;
+    private Long num12;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getNum1() {
+        return num1;
+    }
+
+    public void setNum1(Long num1) {
+        this.num1 = num1;
+    }
+
+    public Long getNum2() {
+        return num2;
+    }
+
+    public void setNum2(Long num2) {
+        this.num2 = num2;
+    }
+
+    public Long getNum3() {
+        return num3;
+    }
+
+    public void setNum3(Long num3) {
+        this.num3 = num3;
+    }
+
+    public Long getNum4() {
+        return num4;
+    }
+
+    public void setNum4(Long num4) {
+        this.num4 = num4;
+    }
+
+    public Long getNum5() {
+        return num5;
+    }
+
+    public void setNum5(Long num5) {
+        this.num5 = num5;
+    }
+
+    public Long getNum6() {
+        return num6;
+    }
+
+    public void setNum6(Long num6) {
+        this.num6 = num6;
+    }
+
+    public Long getNum7() {
+        return num7;
+    }
+
+    public void setNum7(Long num7) {
+        this.num7 = num7;
+    }
+
+    public Long getNum8() {
+        return num8;
+    }
+
+    public void setNum8(Long num8) {
+        this.num8 = num8;
+    }
+
+    public Long getNum9() {
+        return num9;
+    }
+
+    public void setNum9(Long num9) {
+        this.num9 = num9;
+    }
+
+    public Long getNum10() {
+        return num10;
+    }
+
+    public void setNum10(Long num10) {
+        this.num10 = num10;
+    }
+
+    public Long getNum11() {
+        return num11;
+    }
+
+    public void setNum11(Long num11) {
+        this.num11 = num11;
+    }
+
+    public Long getNum12() {
+        return num12;
+    }
+
+    public void setNum12(Long num12) {
+        this.num12 = num12;
+    }
+}
